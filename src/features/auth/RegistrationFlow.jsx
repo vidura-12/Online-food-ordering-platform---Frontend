8de55dd5497ac07355e62ec979dacf806ec7ef96
vidura-12 <viduranirmal195@gmail.com>
2026-03-28 16:48:38 +0530

    Update RegistrationFlow.jsx

--- src/features/auth/RegistrationFlow.jsx
+++ src/features/auth/RegistrationFlow.jsx
@@ -36,7 +36,7 @@ const RegistrationFlow = () => {
     }
   
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("http://localhost:5000/gateway/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
